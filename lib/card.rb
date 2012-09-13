a7c3c275710bfc0e23dc6205971f777ade92577b
J Nelson <jeremy.s.nelson@gmail.com>
2012-09-13 10:34:31 -0400

    Card can read/write now.

--- lib/card.rb
+++ lib/card.rb
@@ -12,24 +12,48 @@ class Card
   # Sample card definition follows:
   #
   #Goblin Piledriver
- #1R
- #Creature -- Goblin Warrior
- #1/2
- #Protection from blue
- #Whenever Goblin Piledriver attacks, it gets +2/+0 until end of turn for each other attacking Goblin.
- #ONS-R
+  #1R
+  #Creature -- Goblin Warrior
+  #1/2
+  #Protection from blue
+  #Whenever Goblin Piledriver attacks, it gets +2/+0 until end of turn for each other attacking Goblin.
+  #ONS-R
   def load_from_text (text)
-    lines = text.each_line
+    lines = text.each_line.map { |l| l.chomp }
     @name = lines[0]
     @cost = lines[1]
     @types = lines[2].split
-    if (@types.includes? 'Creature')
+    if (@types.include? 'Creature')
       (@power, @toughness) = lines[3].split '/'
-      @text = lines[4..-2]
+      @text = lines[4..-2].join("\n")
     else
-      @text = lines[3..-2]
+      @text = lines[3..-2].join("\n")
     end
     (@expansion, @rarity) = lines[-1].split '-'
+
+  end
+
+  # Format, where <> denotes mandatory, and [] denotes optional.
+  # ----BEGIN FORMAT EXAMPLE----
+  # <Name>
+  # <Cost>
+  # [Supertype] <Type> [-- [Optional] [Subtypes]]
+  # [Power/Toughness]
+  # [Text]
+  # [Additional Text]
+  # [More text]
+  # <Expansion>-<Rarity>
+  #
+  # ----END FORMAT EXAMPLE----
+  # Note: If there is no power/toughness, omit the line entirely
+  # Same with text lines.
+  def to_s
+    text = [@name, @cost].join("\n") + "\n"
+    text << @types.join(' ') + "\n"
+    text << "#{@power}/#{@toughness}\n" unless @power.nil? and @toughness.nil?
+    text << "#{@text}\n"
+    text << "#{@expansion}-#{@rarity}\n"
+
 
   end
 
